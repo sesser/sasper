6830c23337e7ed413fdf9610040caabb2a9d3883
Randy <rsesser@local.com>
2016-12-02 13:40:25 -0800

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,56 @@
+/**
+ * Main JS file for Casper behaviours
+ */
+
+/* globals jQuery, document */
+(function ($, undefined) {
+    "use strict";
+
+    var $document = $(document);
+
+    $document.ready(function () {
+
+        var $postContent = $(".post-content");
+        $postContent.fitVids();
+
+        $(".scroll-down").arctic_scroll();
+
+        $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
+            e.preventDefault();
+            $("body").toggleClass("nav-opened nav-closed");
+        });
+
+    });
+
+    // Arctic Scroll by Paul Adam Davis
+    // https://github.com/PaulAdamDavis/Arctic-Scroll
+    $.fn.arctic_scroll = function (options) {
+
+        var defaults = {
+            elem: $(this),
+            speed: 500,
+            scroll_selector: 'html,body'
+        };
+
+        var options = $.extend(defaults, options),
+            to_scroll = options.scroll_selector;
+
+        options.elem.click(function(event){
+            console.log(options.scroll_selector);
+            event.preventDefault();
+            var offset = ($(this).attr('data-offset')) ? $(this).attr('data-offset') : false,
+                position = ($(this).attr('data-position')) ? $(this).attr('data-position') : false;
+
+            if (offset) {
+                var toMove = parseInt(offset);
+                $(options.scroll_selector).stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, options.speed);
+            } else if (position) {
+                var toMove = parseInt(position);
+                $(options.scroll_selector).stop(true, false).animate({scrollTop: toMove }, options.speed);
+            } else {
+                $(options.scroll_selector).stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, options.speed);
+            }
+        });
+
+    };
+})(jQuery);
